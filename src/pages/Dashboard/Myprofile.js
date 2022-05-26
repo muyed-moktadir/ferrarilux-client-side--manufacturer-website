4a1db133f80c24fe365d29f028a19c81aedd2a85
muyed moktadir <muyed004@gmail.com>
2022-05-26 21:29:43 +0600

    MY PROFILE

--- src/pages/Dashboard/Myprofile.js
+++ src/pages/Dashboard/Myprofile.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Myprofile = () => {
-    return (
-        <div><h2>my profile</h2>
-        </div>
-    );
+  return (
+    <div class="card card-side bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Name: Muyed Moktadir</h2>
+    <h3><strong>Email Address</strong>: muyed004@gmail.com</h3>
+    <h3><strong>Educational Background</strong>: Graduated From East West University</h3>
+    <h3><strong>skills As Web Developer</strong>: HTML,CSS,JavaScript,React,Firebase Authentication,FireBase Hook,node js</h3>
+    <h3><strong>FrameWork</strong>: tailwind,daisy ui, BootStrap</h3>
+     <strong>Heres My Project Live Link</strong>
+      <h3>First:<a target='_blank' href="https://bikevio-newproject.web.app/"> bikevio </a> </h3>
+      <h3>Second :<a target='_blank' href="https://travel-trek-e5c58.web.app/"> Travel Trek</a></h3>
+      <h3>Third: <a target='_blank' href="https://starlit-melomakarona-1635ec.netlify.app/"> bikevio </a> </h3>     
+  </div>
+</div>
+  );
 };
 
 export default Myprofile;
